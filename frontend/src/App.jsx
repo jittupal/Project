1786cca2cba649664ddc3af8eae0ec6b58bcb8f2
jitendra kumar <128616359+jittupal@users.noreply.github.com>
2025-04-02 10:09:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.baseURL = 'https://project-1-jibe.onrender.com';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
